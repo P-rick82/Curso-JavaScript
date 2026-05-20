0ae202adae251bd255dd65e7916295aaf5dfb82c
P-rick82 <patrikaugustor800@gmail.com>
2026-05-20 16:28:29 -0300

    New things that I learn

---
+++ Aula 20-05/Exercicio/Exer.js
@@ -0,0 +1,20 @@
+// Exercicos
+
+// 1) Soma dos dígitos de um número:
+// Peça ao usuário um número inteiro positivo.
+// Use um `while` para somar seus dígitos.
+
+let n = Number(prompt("Digite um número"))
+
+if(isNaN(n) || isNaN(n2))
+{
+    alert("Você deve digitar os números, não deve escrever (Ex: 5, 6 , 7). Tente novamente!")
+}
+else if (n.isInteger(n) || n2.isInteger(n2))
+{
+    alert("Os números precisa ser inteiro portanto, sem casas decimais (Ex: 20). Tente novamente!")
+}
+else
+{
+
+}
